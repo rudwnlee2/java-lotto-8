1558cb95b90aa88ea2187e6dd857f23359bb4518
rudwnlee2 <lgj0731@naver.com>
2025-10-31 16:46:01 +0900

    refactor: Lotto 도메인 규칙(상수) 정의 및 검증 로직 수정

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,9 +1,15 @@
 package lotto;
 
+import java.util.Collections;
 import java.util.List;
 
 public class Lotto {
     private final List<Integer> numbers;
+
+    public static final int LOTTO_SIZE = 6;
+    public static final int MIN_NUMBER = 1;
+    public static final int MAX_NUMBER = 45;
+    public static final int LOTTO_PRICE = 1000;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
@@ -11,10 +17,22 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
+        if (numbers.size() != LOTTO_SIZE) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
         }
+
+        numberRangeValidate(numbers);
     }
 
     // TODO: 추가 기능 구현
+
+    private void numberRangeValidate(List<Integer> numbers) {
+        int minNumber = Collections.min(numbers);
+        int maxNumber = Collections.max(numbers);
+
+        if (minNumber < MIN_NUMBER || maxNumber > MAX_NUMBER) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 " + MIN_NUMBER + "부터 " + MAX_NUMBER + " 사이여야 합니다.");
+        }
+    }
+
 }
