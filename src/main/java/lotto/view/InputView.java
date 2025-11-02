6847635adbb97a72c429399239a455e443e978b8
rudwnlee2 <lgj0731@naver.com>
2025-11-02 14:19:52 +0900

    feat: 로또 구매 관련 입출력 View 구현

---
+++ src/main/java/lotto/view/InputView.java
@@ -0,0 +1,18 @@
+package lotto.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+
+    public static final String PROMPT_PURCHASE_AMOUNT = "구입금액을 입력해 주세요.";
+    public static final String PROMPT_WINNING_NUMBERS = "당첨 번호를 입력해 주세요.";
+    public static final String PROMPT_BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
+
+    public static int inputPurchaseAmount() {
+        System.out.println(PROMPT_PURCHASE_AMOUNT);
+        int money = Integer.parseInt(Console.readLine());
+        return money;
+    }
+
+
+}
---
+++ src/main/java/lotto/view/OutputView.java
@@ -0,0 +1,25 @@
+package lotto.view;
+
+import lotto.domain.Lotto;
+
+import java.util.List;
+
+public class OutputView {
+
+    public static final String PURCHASE_COUNT_MESSAGE = "%d개를 구매했습니다.";
+    public static final String STATISTICS_HEADER = "당첨 통계";
+    public static final String SEPARATOR = "---";
+    public static final String STATISTICS_RESULT_FORMAT = "%s - %d개";
+    public static final String TOTAL_PROFIT_RATE_MESSAGE = "총 수익률은 %.1f%%입니다.";
+
+    public static void printPurchaseCount(int count) {
+        System.out.println(String.format(PURCHASE_COUNT_MESSAGE, count));
+    }
+
+    public static void printLottos(List<Lotto> lottos) {
+        for (Lotto lotto : lottos) {
+            System.out.println(lotto.getNumbers());
+        }
+    }
+
+}
